b6d519e88034b3d5eda75f80b3b0a9849465d5fd
Prateekp16 <47525892+Prateekp16@users.noreply.github.com>
2020-02-03 15:34:42 +0530

    payment data request added

--- app.js
+++ app.js
@@ -12,3 +12,36 @@ const clientConfiguration = {
 };
 
 googlePayClient.isReadyToPay(clientConfiguration)
+
+const googlePaymentDataRequest = {
+    environment: 'TEST',
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    merchantInfo: {
+      // A merchant ID is available after approval by Google.
+      // @see {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist}
+      // merchantId: '0123456789',
+      merchantName: 'Example Merchant'
+    },
+    allowedPaymentMethods: [{
+      type: 'CARD',
+      parameters: {
+        allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+        allowedCardNetworks: ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+      },
+      tokenizationSpecification: {
+        type: 'PAYMENT_GATEWAY',
+        // Check with your payment gateway on the parameters to pass.
+        // @see {@link https://developers.google.com/pay/api/web/reference/request-objects#gateway}
+        parameters: {
+          'gateway': 'example',
+          'gatewayMerchantId': 'exampleGatewayMerchantId'
+        }
+      }
+    }]
+  };
+  
+  const methodData = [
+    {supportedMethods: 'https://google.com/pay', data: googlePaymentDataRequest},
+    {supportedMethods: 'basic-card'}
+  ];
